9828d8241296b859fb07da910b1975e3ca8c8618
so040602 <so040602@gmail.com>
2024-12-02 16:09:44 +0900

    test

---
+++ src/main/java/com/backend/foodrecipe/FoodrecipeApplication.java
@@ -0,0 +1,14 @@
+package com.backend.foodrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodrecipeApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(FoodrecipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/foodrecipe/FoodrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.foodrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodrecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
